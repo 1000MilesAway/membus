d91d2682910286e147920f73822794a9c7a3a5ec
1000MilesAway <45456072+1000MilesAway@users.noreply.github.com>
2020-08-24 14:24:20 +0700

    Add files via upload

--- client.py
+++ client.py
@@ -6,11 +6,11 @@ client.connect()
 i=1
 while 1:
     try:
-        response = client.read_holding_registers(address=247, count=5, unit=0x01)
+        response = client.read_holding_registers(address=250, count=1, unit=247)
     except:
         print("No connection")
     else:
-        rg = response.registers[3]
+        rg = response.registers[0]
         if rg == 247:
             client.write_register(250, i)
             print("zapisal")
