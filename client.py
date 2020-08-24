5d3e3556e085ccefc27985a23c6360b93350b46b
1000MilesAway <45456072+1000MilesAway@users.noreply.github.com>
2020-08-24 14:04:05 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,20 @@
+from pymodbus.client.sync import ModbusTcpClient
+import time
+
+client = ModbusTcpClient('127.0.0.1')
+client.connect()
+i=1
+while 1:
+    try:
+        response = client.read_holding_registers(address=247, count=5, unit=0x01)
+    except:
+        print("No connection")
+    else:
+        rg = response.registers[3]
+        if rg == 247:
+            client.write_register(250, i)
+            print("zapisal")
+        i += 1
+    time.sleep(1)
+
+client.close()
